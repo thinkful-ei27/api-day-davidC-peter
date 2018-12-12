53cbea3f535693c1b808a46cc00af12520b61401
dhcohn <davidhcohn@gmail.com>
2018-12-12 12:54:49 -0500

    up to step 4

--- scripts/api.js
+++ scripts/api.js
@@ -6,12 +6,32 @@ const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/davidcpeter'
   
   const getItems = function (callback) {
-    callback('api module works');
-  }
+    $.getJSON(`
+    ${BASE_URL}/items
+    `, callback);
+
+  };
+  const createItem = function(name, callback){
+    const newItem = JSON.stringify({
+      name: name,
+
+    });
+    $.ajax(
+      {
+        url: `${BASE_URL}/items`,
+        method: 'POST',
+        contentType: 'application/json',
+        data: newItem,
+        success: callback,
+      }
+    );
+  };
 
   return {
     getItems,
-
+    createItem,
   };
+
+
 
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,13 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
+
+api.getItems();
+
+api.createItem('pears', (newItem) => {
+  api.getItems((items) => {
+    console.log(items);
+  });
+});
+
+
